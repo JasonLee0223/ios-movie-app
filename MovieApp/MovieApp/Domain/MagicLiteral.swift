35966467988ea2f554d8cb41ce2d91c5283ae4d7
Jason <ggj00125@gmail.com>
2023-05-17 20:05:35 +0900

    [Chore]: 타입별로 파일 분리

--- MovieApp/MovieApp/Domain/MagicLiteral.swift
+++ MovieApp/MovieApp/Domain/MagicLiteral.swift
@@ -29,22 +29,3 @@ enum MagicLiteral {
         static let genre = "장르별 영화"
     }
 }
-
-enum MagicNumber {
-    static let zero: CGFloat = 0
-    
-    enum Attributes {
-        static let genreTitleFont: CGFloat = 18
-        static let spcing: CGFloat = 12
-        static let fontSize: CGFloat = 14
-        static let imageCornerRadius: CGFloat = 10
-    }
-    
-    enum Size {
-        static let introducePosterWidth: CGFloat = 140
-        static let introducePosterHeight: CGFloat = 198
-        
-        static let genrePosterWidth: CGFloat = 104
-        static let genrePosterHeight: CGFloat = 104
-    }
-}
