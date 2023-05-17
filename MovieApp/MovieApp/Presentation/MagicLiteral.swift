13968eec18ee9bd35c4e3946d4448c0f8d3d6494
Jason <ggj00125@gmail.com>
2023-05-17 14:04:27 +0900

    [Feat]: MagicLiteral 파일 생성 및 타입 구현

---
+++ MovieApp/MovieApp/Presentation/MagicLiteral.swift
@@ -0,0 +1,18 @@
+//
+//  MagicLiteral.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/17.
+//
+
+import Foundation
+
+enum MagicLiteral {
+    static let zero = "0"
+    static let newMovie = "신작"
+    static let upTriangle = "arrowtriangle.up.fill"
+    static let downTriangle = "arrowtriangle.down.fill"
+    static let dateFormat = "yyyy-MM-dd"
+    static let todayAudience = "오늘 "
+    static let totalAudience = " / 총 "
+}
