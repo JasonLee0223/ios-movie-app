25263c2046469df8a7dc07698dd8ab06399c058d
Jason <ggj00125@gmail.com>
2023-05-17 13:14:48 +0900

    [Feat]: MovieDetailInformation 그룹 내부 타입 구현

---
+++ MovieApp/MovieApp/Data/DataStructure/Response/MovieDetailInformation/MovieDetailInformation.swift
@@ -0,0 +1,12 @@
+//
+//  MovieDetailInformation.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/17.
+//
+
+import Foundation
+
+struct MovieDetailInformation: Decodable {
+    let movieInfoResult: MovieInfoResult
+}
