00fe5ce5e2d75974806c439d8b87d48d81ea367d
Jason <ggj00125@gmail.com>
2023-05-17 12:59:38 +0900

    [Feat]: Basic 파일 생성 및 타입 구현

---
+++ MovieApp/MovieApp/Infrastructure/Network/Query/Basic.swift
@@ -0,0 +1,13 @@
+//
+//  Basic.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/17.
+//
+
+import Foundation
+
+enum Basic {
+    static let baseURL: String = "https://www.kobis.or.kr/kobisopenapi/webservice/rest"
+    static let format: String = ".json"
+}
