0f1972aa587c372527933f3400434eeb56437085
Jason <ggj00125@gmail.com>
2023-05-17 13:00:55 +0900

    [Feat]: Show 파일 생성 및 타입 구현

---
+++ MovieApp/MovieApp/Infrastructure/Network/Query/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/17.
+//
+
+import Foundation
+
+enum Movie {
+    static let category: String = "/movie"
+    static let searchMovieInfo: String = "/searchMovieInfo"
+}
