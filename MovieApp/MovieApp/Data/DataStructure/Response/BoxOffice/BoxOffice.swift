c6c47f48f99e951e61828f760343bda7ac2a5e4a
Jason <ggj00125@gmail.com>
2023-05-17 13:10:55 +0900

    [Feat]: API BoxOffice JSON 데이터 매핑 타입 정의

---
+++ MovieApp/MovieApp/Data/DataStructure/Response/BoxOffice/BoxOffice.swift
@@ -0,0 +1,12 @@
+//
+//  BoxOffice.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/17.
+//
+
+import Foundation
+
+struct BoxOffice: Decodable {
+    let boxOfficeResult: BoxOfficeResult
+}
