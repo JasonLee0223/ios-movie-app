661fadc9784012f982af7c3539dd274b5277b01d
Jason <ggj00125@gmail.com>
2023-05-17 13:01:51 +0900

    [Feat]: APIMagicLiteral 파일 생성 및 타입 구현

---
+++ MovieApp/MovieApp/Infrastructure/Network/Query/APIMagicLiteral.swift
@@ -0,0 +1,21 @@
+//
+//  APIMagicLiteral.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/17.
+//
+
+import Foundation
+
+enum APIMagicLiteral {
+    static let key = getAPIKEY()
+    static let targetQuery = "targetDt"
+    static let movieCode = "movieCd"
+    
+    static func getAPIKEY() -> String {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "BoxOfficeAPIKEY") as? String else {
+            return ""
+        }
+        return apiKey
+    }
+}
