b3f83f0a607a9ffcd0ca26c176d73bc26edc1f2b
Jason <ggj00125@gmail.com>
2023-05-19 13:07:41 +0900

    [Refactor]: CollectionView CompositionalLayout 리펙토링

--- MovieApp/MovieApp/Presentation/CellLayout/HomeViewLayout.swift
+++ MovieApp/MovieApp/Presentation/CellLayout/HomeViewLayout.swift
@@ -32,49 +32,81 @@ struct HomeViewLayout {
     
     func createIntroduceCellCompositionalLayout() -> NSCollectionLayoutSection? {
         
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(MagicNumber.RelatedToLayout.introduceCellItemFractionalWidthFraction),
-                                              
-                                              heightDimension: .fractionalHeight(MagicNumber.RelatedToLayout.fractionalDefaultFraction))
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .estimated(191),
+            heightDimension: .estimated(64)
+        )
+        
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .topLeading
+        )
+        
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
-        item.contentInsets = NSDirectionalEdgeInsets(top: MagicNumber.RelatedToLayout.itemInset,
-                                                     leading: MagicNumber.RelatedToLayout.itemInset,
-                                                     bottom: MagicNumber.RelatedToLayout.itemInset,
-                                                     trailing: MagicNumber.RelatedToLayout.itemInset)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0 / 2.3),
+            heightDimension: .fractionalHeight(1.0 / 2.6)
+        )
         
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(MagicNumber.RelatedToLayout.fractionalDefaultFraction),
-                                               heightDimension: .fractionalHeight(MagicNumber.RelatedToLayout.fractionalDefaultFraction))
-        let group = NSCollectionLayoutGroup(layoutSize: groupSize)
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        group.contentInsets = .init(top: 0, leading: 20, bottom: 40, trailing: 0)
         
         let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: MagicNumber.RelatedToLayout.itemInset,
-                                                        leading: MagicNumber.RelatedToLayout.itemInset,
-                                                        bottom: MagicNumber.RelatedToLayout.itemInset,
-                                                        trailing: MagicNumber.RelatedToLayout.itemInset)
+        section.boundarySupplementaryItems = [header]
+        section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
         
         return section
     }
     
     func createGenreCellCompositionalLayout() -> NSCollectionLayoutSection? {
         
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(MagicNumber.RelatedToLayout.GenreCellItemFractionalWidthFraction),
-                                              heightDimension: .fractionalHeight(MagicNumber.RelatedToLayout.fractionalDefaultFraction))
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .estimated(127),
+            heightDimension: .estimated(56)
+        )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .topLeading
+        )
+        
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension:.fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
-        item.contentInsets = NSDirectionalEdgeInsets(top: MagicNumber.RelatedToLayout.itemInset,
-                                                     leading: MagicNumber.RelatedToLayout.itemInset,
-                                                     bottom: MagicNumber.RelatedToLayout.itemInset,
-                                                     trailing: MagicNumber.RelatedToLayout.itemInset)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(MagicNumber.RelatedToLayout.fractionalDefaultFraction),
-                                               heightDimension: .fractionalHeight(MagicNumber.RelatedToLayout.fractionalDefaultFraction))
-        let group = NSCollectionLayoutGroup(layoutSize: groupSize)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0 / 3.0),
+            heightDimension: .fractionalHeight(1.0 / 5.5)
+//            heightDimension: .fractionalHeight(1.0 / 1.5)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        group.contentInsets = .init(top: 0, leading: 10, bottom: 24, trailing: 10)
         
         let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: MagicNumber.RelatedToLayout.itemInset,
-                                                        leading: MagicNumber.RelatedToLayout.itemInset,
-                                                        bottom: MagicNumber.RelatedToLayout.itemInset,
-                                                        trailing: MagicNumber.RelatedToLayout.itemInset)
+        section.contentInsets = NSDirectionalEdgeInsets(
+            top: MagicNumber.RelatedToLayout.itemInset,
+            leading: MagicNumber.RelatedToLayout.itemInset,
+            bottom: MagicNumber.RelatedToLayout.itemInset,
+            trailing: MagicNumber.RelatedToLayout.itemInset
+        )
+        section.boundarySupplementaryItems = [header]
+        section.orthogonalScrollingBehavior = .continuous
+        
         return section
     }
 }
