42d085ad062ee0be640837f3add24add56b0c63e
Jason <ggj00125@gmail.com>
2023-05-22 15:17:49 +0900

    [Refactor]: 필요없는 코드 삭제 및 KakaoMagicLiteral 프로퍼티로 적용

--- MovieApp/MovieApp/Infrastructure/Network/Query/KaKao/KakaoBasic.swift
+++ MovieApp/MovieApp/Infrastructure/Network/Query/KaKao/KakaoBasic.swift
@@ -9,12 +9,10 @@ import Foundation
 
 enum KakaoBasic {
     // https://dapi.kakao.com/v2/search/image?query=스즈메의문단속
-    static let baseURL: String = "https://dapi.kakao.com/v2/search/image"
-    static let query: String = "query"
-    static let page: String = "page"
-    static let size: String = "size"
+    static let baseURL: String = KakaoMagicLiteral.baseURL
+    static let query: String = KakaoMagicLiteral.query
     
     static let headers: [String: String] = [
-        "Authorization":"KakaoAK fa7f07de12d5091df52431ed3f1c700d"
+        KakaoMagicLiteral.authorization: KakaoMagicLiteral.kakaoRESTAPIKey
     ]
 }
