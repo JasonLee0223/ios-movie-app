007884dc49c84210a24aabb74a38f7f8aa59dc19
Jason <ggj00125@gmail.com>
2023-05-22 15:17:20 +0900

    [Feat]: KakaoMagicLiteral 타입 구현

---
+++ MovieApp/MovieApp/Infrastructure/Network/Query/KaKao/KakaoMagicLiteral.swift
@@ -0,0 +1,23 @@
+//
+//  KakaoMagicLiteral.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/22.
+//
+
+import Foundation
+
+enum KakaoMagicLiteral {
+    static let baseURL: String = "https://dapi.kakao.com/v2/search/image"
+    static let query: String = "query"
+    static let authorization: String = "Authorization"
+    static let kakaoRESTAPIKey: String = "KakaoAK \(kakaoApiKey)"
+    private static let kakaoApiKey: String = getRESTAPIKEY()
+    
+    private static func getRESTAPIKEY() -> String {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "KakaoRESTAPIKEY") as? String else {
+            return ""
+        }
+        return apiKey
+    }
+}
