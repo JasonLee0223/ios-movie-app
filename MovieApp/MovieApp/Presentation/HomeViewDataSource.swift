9d371e38db7c999d0ddc91d5513ce4edf98a8114
Jason <ggj00125@gmail.com>
2023-05-22 22:25:53 +0900

    [Style]: 컨벤션 적용

--- MovieApp/MovieApp/Presentation/HomeViewDataSource.swift
+++ MovieApp/MovieApp/Presentation/HomeViewDataSource.swift
@@ -56,10 +56,12 @@ final class HomeViewDataSource: NSObject, UICollectionViewDataSource {
                         at indexPath: IndexPath) -> UICollectionReusableView {
         
         switch kind {
+            
         case UICollectionView.elementKindSectionHeader:
-            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                                   withReuseIdentifier: HomeHeaderView.identifier,
-                                                                                   for: indexPath) as? HomeHeaderView else {
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: HomeHeaderView.identifier,
+                for: indexPath) as? HomeHeaderView else {
                 return UICollectionReusableView()
             }
             
@@ -84,20 +86,26 @@ final class HomeViewDataSource: NSObject, UICollectionViewDataSource {
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         switch mockData[indexPath.section] {
+            
         case let .introduce(items):
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieIntroduceCell.identifier,
-                                                                for: indexPath) as? MovieIntroduceCell else {
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: MovieIntroduceCell.identifier,
+                for: indexPath) as? MovieIntroduceCell else {
                 return UICollectionViewCell()
             }
+            
             let item = items[indexPath.item]
             cell.setPoster(with: item.posterImage)
             cell.setPoster(with: item.posterName)
             return cell
+            
         case let .Genre(items):
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieGenreCell.identifier,
-                                                                for: indexPath) as? MovieGenreCell else {
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: MovieGenreCell.identifier,
+                for: indexPath) as? MovieGenreCell else {
                 return UICollectionViewCell()
             }
+            
             let item = items[indexPath.item]
             cell.setGenrePoster(with: item.genreImage)
             cell.setGenreType(with: item.genreName)
