a04039202d7b66ebeee2425c3b535d62795576f5
Jason <ggj00125@gmail.com>
2023-05-22 15:30:55 +0900

    [Refactor]: baseURL에서 firstPath인 image 프로퍼티 분리

--- MovieApp/MovieApp/Infrastructure/Network/Query/KaKao/KakaoBasic.swift
+++ MovieApp/MovieApp/Infrastructure/Network/Query/KaKao/KakaoBasic.swift
@@ -10,6 +10,7 @@ import Foundation
 enum KakaoBasic {
     // https://dapi.kakao.com/v2/search/image?query=스즈메의문단속
     static let baseURL: String = KakaoMagicLiteral.baseURL
+    static let image: String = KakaoMagicLiteral.image
     static let query: String = KakaoMagicLiteral.query
     
     static let headers: [String: String] = [
--- MovieApp/MovieApp/Infrastructure/Network/Query/KaKao/KakaoMagicLiteral.swift
+++ MovieApp/MovieApp/Infrastructure/Network/Query/KaKao/KakaoMagicLiteral.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 enum KakaoMagicLiteral {
-    static let baseURL: String = "https://dapi.kakao.com/v2/search/image"
+    static let baseURL: String = "https://dapi.kakao.com/v2/search"
+    static let image: String = "/image"
     static let query: String = "query"
     static let authorization: String = "Authorization"
     static let kakaoRESTAPIKey: String = "KakaoAK \(kakaoApiKey)"
