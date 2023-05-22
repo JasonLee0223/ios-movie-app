285481fa01aa19219d0ac817e15dbeca08b3796d
Jason <ggj00125@gmail.com>
2023-05-22 15:23:08 +0900

    [Feat]: MoviePosterImage API Response 타입 구현

---
+++ MovieApp/MovieApp/Data/DataStructure/Response/Kakao/MoviePosterImage.swift
@@ -0,0 +1,19 @@
+//
+//  MoviePosterImage.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/22.
+//
+
+import Foundation
+
+// MARK: - Welcome4
+struct MoviePosterImage {
+    let documents: [Document]
+}
+
+// MARK: - Document
+struct Document {
+    let imageURL: String
+    let thumbnailURL: String
+}
