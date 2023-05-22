2321d74dce05c9daad02e407711ac2053a955df8
Jason <ggj00125@gmail.com>
2023-05-22 16:49:04 +0900

    [Refactor]: CodingKeys 추가

--- MovieApp/MovieApp/Data/DataStructure/Response/Kakao/MoviePosterImage.swift
+++ MovieApp/MovieApp/Data/DataStructure/Response/Kakao/MoviePosterImage.swift
@@ -8,11 +8,16 @@
 import Foundation
 
 
-struct MoviePosterImage {
+struct MoviePosterImage: Decodable {
     let documents: [Document]
 }
 
-struct Document {
+struct Document: Decodable {
     let imageURL: String
     let thumbnailURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case imageURL = "image_url"
+        case thumbnailURL = "thumbnail_url"
+    }
 }
