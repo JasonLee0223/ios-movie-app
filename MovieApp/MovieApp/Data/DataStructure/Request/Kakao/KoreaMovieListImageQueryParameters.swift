bb3674e233684ba131f777b89dbabb378d716678
Jason <ggj00125@gmail.com>
2023-05-22 15:19:25 +0900

    [Feat]: KoreaMovieListImageQueryParameters 타입 구현

---
+++ MovieApp/MovieApp/Data/DataStructure/Request/Kakao/KoreaMovieListImageQueryParameters.swift
@@ -0,0 +1,12 @@
+//
+//  KoreaMovieListImageQueryParameters.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/22.
+//
+
+import Foundation
+
+struct KoreaMovieListImageQueryParameters: Encodable {
+    let query = KakaoMagicLiteral.query
+}
