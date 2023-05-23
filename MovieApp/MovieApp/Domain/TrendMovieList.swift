4ad0990b671066ecb9b7982b352fe736a4425905
Jason <ggj00125@gmail.com>
2023-05-23 15:17:03 +0900

    [Feat]: API에서 사용될 TrendMovieList Entity 타입 구현

---
+++ MovieApp/MovieApp/Domain/TrendMovieList.swift
@@ -0,0 +1,13 @@
+//
+//  TrendMovieList.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/23.
+//
+
+import Foundation
+
+struct TrendMovieList {
+    let posterImagePath: String
+    let posterName: String
+}
