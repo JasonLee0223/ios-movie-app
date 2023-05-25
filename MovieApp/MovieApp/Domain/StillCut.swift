d7e42070ed2bbf97fbe120888526aa934de1d2a9
Jason <ggj00125@gmail.com>
2023-05-25 22:10:42 +0900

    [Refactor]: Data 타입으로 변경

--- MovieApp/MovieApp/Domain/StillCut.swift
+++ MovieApp/MovieApp/Domain/StillCut.swift
@@ -10,7 +10,7 @@ import Foundation
 struct StillCut: BusinessModel {
     var identifier: UUID
     
-    let genreImagePath: String
+    let genreImagePath: Data
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(identifier)
