23a3ea22e71195231f986895a23d0756f7ae13ce
Jason <ggj00125@gmail.com>
2023-05-25 18:56:17 +0900

    [Feat]: BusinessModelWrapper 타입 구현

---
+++ MovieApp/MovieApp/Domain/BusinessModelWrapper.swift
@@ -0,0 +1,46 @@
+//
+//  BusinessModelWrapper.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/25.
+//
+
+import Foundation
+
+enum BusinessModelWrapper: Hashable {
+    case trendMovie(TrendMovie)
+    case stillCut(StillCut)
+    case koreaBoxOfficeList(KoreaBoxOfficeList)
+    
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .trendMovie(let item):
+            hasher.combine(item)
+            
+        case .stillCut(let item):
+            hasher.combine(item)
+            
+        case .koreaBoxOfficeList(let item):
+            hasher.combine(item)
+        }
+    }
+    
+    static func ==(lhs: BusinessModelWrapper, rhs: BusinessModelWrapper) -> Bool {
+        switch (lhs, rhs) {
+        case (
+            .trendMovie(let lhsItem), .trendMovie(let rhsItem)
+        ): return lhsItem == rhsItem
+            
+        case (
+            .stillCut(let lhsItem), .stillCut(let rhsItem)
+        ): return lhsItem == rhsItem
+            
+        case (
+            .koreaBoxOfficeList(let lhsItem), .koreaBoxOfficeList(let rhsItem)
+        ): return lhsItem == rhsItem
+        
+        default:
+            return false
+        }
+    }
+}
