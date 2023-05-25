9bd42506d21c14ba70258af8dc06681691cb6cde
Jason <ggj00125@gmail.com>
2023-05-25 21:13:45 +0900

    [Refactor]: T의 타입을 Optional(T)로 변경

--- MovieApp/MovieApp/Presentation/ViewModel/Observable.swift
+++ MovieApp/MovieApp/Presentation/ViewModel/Observable.swift
@@ -9,22 +9,21 @@ import Foundation
 
 final class Observable<T> {
     
-    typealias Listener = (T) -> Void
+    typealias Listener = (T?) -> Void
     
     var listener: Listener?
     
-    var value: T {
+    var value: T? {
         didSet {
             listener?(value)
         }
     }
     
-    init(_ value: T) {
+    init(_ value: T? = nil) {
         self.value = value
     }
     
     func bind(listener: Listener?) {
         self.listener = listener
-        listener?(value)
     }
 }
