9f615199a03bed184d2c8f98ff51746a41932b5f
Jason <ggj00125@gmail.com>
2023-05-28 00:44:41 +0900

    [Feat]: HomeViewDelegate 파일 생성 및 초안 구현

---
+++ MovieApp/MovieApp/Presentation/HomeViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewDelegate.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/28.
+//
+
+import UIKit
+
+final class HomeViewDelegate: NSObject, UICollectionViewDelegate {
+    
+}
