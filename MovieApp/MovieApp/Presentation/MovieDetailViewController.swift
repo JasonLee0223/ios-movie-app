cb8239a6ac7746c0afc09e2920f0e41493fcd46c
Jason <ggj00125@gmail.com>
2023-05-28 02:40:42 +0900

    [Feat]: MovieDetailViewController 초안 작성

--- MovieApp/MovieApp/Presentation/MovieDetailViewController.swift
+++ MovieApp/MovieApp/Presentation/MovieDetailViewController.swift
@@ -12,5 +12,30 @@ final class MovieDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureOfmovieDetailCollectionView()
+        configureHierarchy()
+        print("나 왔어!")
+    }
+    
+    private let movieDetailCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    private func configureHierarchy() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        self.view.addSubview(movieDetailCollectionView)
+        
+        movieDetailCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            movieDetailCollectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            movieDetailCollectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            movieDetailCollectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            movieDetailCollectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+    
+    private func configureOfmovieDetailCollectionView() {
+        movieDetailCollectionView.isScrollEnabled = true
+        movieDetailCollectionView.clipsToBounds = false
+        movieDetailCollectionView.backgroundColor = .black
     }
 }
