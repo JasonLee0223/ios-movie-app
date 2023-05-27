867d926e859ab0c2620f71a535245342f012e3c9
Jason <ggj00125@gmail.com>
2023-05-28 01:02:38 +0900

    [Feat]: MovieDetailViewController 파일 생성 및 타입 정의

---
+++ MovieApp/MovieApp/Presentation/MovieDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailViewController.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/28.
+//
+
+import UIKit
+
+final class MovieDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
