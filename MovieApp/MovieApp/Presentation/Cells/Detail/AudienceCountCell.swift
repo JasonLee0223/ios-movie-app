c4e9c5c1fb22df57d14b20d5a5491435d51b1b20
Jason <ggj00125@gmail.com>
2023-05-28 20:15:09 +0900

    [Feat]: AudienceCount 파일 생성 및 타입 초기 구현

---
+++ MovieApp/MovieApp/Presentation/Cells/Detail/AudienceCountCell.swift
@@ -0,0 +1,23 @@
+//
+//  AudienceCountCell.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/28.
+//
+
+import UIKit
+
+final class AudienceCountCell: UICollectionViewCell, ReusableCell {
+    
+    //MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+    }
+}
