955e5991907132844217a91d8e588183051d9139
Jason <ggj00125@gmail.com>
2023-05-28 20:18:44 +0900

    [Feat]: DetailSectionList 파일 생성 및 타입 구현

---
+++ MovieApp/MovieApp/Presentation/CellLayout/DetailSectionList.swift
@@ -0,0 +1,14 @@
+//
+//  DetailSectionList.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/28.
+//
+
+import Foundation
+
+enum DetailSectionList: Int, CaseIterable, Hashable {
+    case movieDetailInformationSection = 0
+    case movieOfficialsSection
+    case audienceCountSection
+}
