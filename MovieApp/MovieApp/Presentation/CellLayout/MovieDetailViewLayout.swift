81b4a8f4194767ca3592de0b4bb9be9d019e9993
Jason <ggj00125@gmail.com>
2023-05-28 14:53:58 +0900

    [Feat]: MovieDetailViewLayout 파일 생성 및 타입 초기 선언

---
+++ MovieApp/MovieApp/Presentation/CellLayout/MovieDetailViewLayout.swift
@@ -0,0 +1,21 @@
+//
+//  MovieDetailViewLayout.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/28.
+//
+
+import UIKit
+
+struct MovieDetailViewLayout {
+    
+    private let sectionIndex: Int
+    
+    init(sectionIndex: Int) {
+        self.sectionIndex = sectionIndex
+    }
+    
+    func createMovieDetailCellCompositionalLayout() {
+        
+    }
+}
