92d00811470ade68c1f28b558c2d9ff11729b953
Jason <ggj00125@gmail.com>
2023-05-28 22:51:53 +0900

    [Feat]: TMDBMovieDetail DTO 타입 구현

---
+++ MovieApp/MovieApp/Data/DataStructure/Response/TVDBPopular/TMDBMovieDetail.swift
@@ -0,0 +1,45 @@
+//
+//  TMDBMovieDetail.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/28.
+//
+
+import Foundation
+
+struct TMDBMovieDetail: Decodable {
+    let genres: [Genre]
+    let movieIdentifier: Int
+    let movieEnglishTitle: String
+    let overview: String
+    let productionCountries: [ProductionCountry]
+    let releaseDate: String
+    let runtime: Int
+    let summary: String
+    let koreanTitle: String
+    let voteAverage: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case genres
+        case movieIdentifier = "id"
+        case movieEnglishTitle = "originalTitle"
+        case overview
+        case productionCountries
+        case releaseDate
+        case runtime
+        case summary = "tagline"
+        case koreanTitle = "title"
+        case voteAverage
+    }
+    
+    // MARK: - Genre
+    internal struct Genre: Decodable {
+        let id: Int
+        let name: String
+    }
+
+    // MARK: - ProductionCountry
+    internal struct ProductionCountry: Decodable {
+        let iso3166_1, name: String
+    }
+}
