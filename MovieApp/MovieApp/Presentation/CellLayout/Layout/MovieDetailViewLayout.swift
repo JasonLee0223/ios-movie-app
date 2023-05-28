d87227c2a62d8e97650d94d4838d47fb0d55e704
Jason <ggj00125@gmail.com>
2023-05-28 20:31:56 +0900

    [Feat]: MovieDetailViewLayout 타입 구현

--- MovieApp/MovieApp/Presentation/CellLayout/Layout/MovieDetailViewLayout.swift
+++ MovieApp/MovieApp/Presentation/CellLayout/Layout/MovieDetailViewLayout.swift
@@ -9,13 +9,104 @@ import UIKit
 
 struct MovieDetailViewLayout {
     
-    private let sectionIndex: Int
-    
+    //MARK: - Initializer
     init(sectionIndex: Int) {
         self.sectionIndex = sectionIndex
     }
     
-    func createMovieDetailCellCompositionalLayout() {
-        
+    //MARK: - [Public Method] Configure of Layout
+    func create() -> NSCollectionLayoutSection? {
+        let index = DetailSectionList.allCases[sectionIndex]
+        switch index {
+        case .movieDetailInformationSection:
+            return createMovieDetailCellCompositionalLayout()
+        case .movieOfficialsSection:
+            return createMovieOfficialsCellCompositionalLayout()
+        case .audienceCountSection:
+            return createAudienceCountCellCompositionalLayout()
+        }
     }
+    
+    private let sectionIndex: Int
+}
+
+//MARK: - [Private Method] Configure of Section Layout
+extension MovieDetailViewLayout {
+    
+    private func createMovieDetailCellCompositionalLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(1.5)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let group = NSCollectionLayoutGroup(layoutSize: groupSize)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .paging
+        
+        return section
+    }
+    
+    private func createMovieOfficialsCellCompositionalLayout() -> NSCollectionLayoutSection {
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(0.1)
+        )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .topLeading
+        )
+        
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(1.0 / 2.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let group = NSCollectionLayoutGroup(layoutSize: groupSize)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        
+        return section
+    }
+    
+    private func createAudienceCountCellCompositionalLayout() -> NSCollectionLayoutSection {
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(0.1)
+        )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .topLeading
+        )
+        
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(0.75)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let group = NSCollectionLayoutGroup(layoutSize: groupSize)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+    }
+    
 }
