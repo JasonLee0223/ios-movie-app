c6c37748ba1e2cdb98a9171f7dbd58b5b3341db8
Jason <ggj00125@gmail.com>
2023-05-28 23:00:26 +0900

    [Feat]: receiveMovieDetail(with movieDetailRequestDTO: , movieID: ) 메서드 구현

--- MovieApp/MovieApp/Data/Network/APIEndponit/TVDBAPIEndPoint.swift
+++ MovieApp/MovieApp/Data/Network/APIEndponit/TVDBAPIEndPoint.swift
@@ -12,6 +12,15 @@ struct TVDBAPIEndPoint {
     static func receiveWeakTrendingList(with weakMovieListRequestDTO: TMDBQueryParameters) -> EndPoint<TMDBPopular> {
         return EndPoint(baseURL: TVDBBasic.baseURL,
                         firstPath: TVDBBasic.pathQueryOfWeak,
-                        queryParameters: weakMovieListRequestDTO)
+                        queryParameters: weakMovieListRequestDTO
+        )
+    }
+    
+    static func receiveMovieDetail(with movieDetailRequestDTO: TMDBQueryParameters,
+                                   movieID: String) -> EndPoint<TMDBMovieDetail> {
+        return EndPoint(baseURL: TVDBBasic.movieDetailBaseURL,
+                        firstPath: movieID,
+                        queryParameters: movieDetailRequestDTO
+        )
     }
 }
