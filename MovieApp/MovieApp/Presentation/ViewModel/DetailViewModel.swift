baa8c4dd361331f34cd45d3cec453596552270b7
Jason <ggj00125@gmail.com>
2023-05-30 19:25:51 +0900

    [Feat]: DetailViewModel 파일 생성 및 타입 구현

---
+++ MovieApp/MovieApp/Presentation/ViewModel/DetailViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewModel.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/05/30.
+//
+
+import Foundation
+
+final class DetailViewModel {
+    
+    init() {
+        self.detailLoader = DetailLoader()
+    }
+    
+    private let detailLoader: DetailLoader
+}
+
+extension DetailViewModel {
+    
+}
