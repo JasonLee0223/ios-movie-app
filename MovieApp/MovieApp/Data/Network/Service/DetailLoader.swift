39690e11e25ba64f11e6afaffdc0afb2a3b8add1
Jason <ggj00125@gmail.com>
2023-05-31 00:34:17 +0900

    [Chore]: DetailLoader로 코드 이동

--- MovieApp/MovieApp/Data/Network/Service/DetailLoader.swift
+++ MovieApp/MovieApp/Data/Network/Service/DetailLoader.swift
@@ -20,6 +20,7 @@ final class DetailLoader {
     private let networkService: NetworkService
 }
 
+//MARK: - [Public Method] Use at DetailViewModel
 extension DetailLoader {
     
     func loadMovieDetailInformation(movieCode: String) async throws -> TMDBMovieDetail {
@@ -46,5 +47,64 @@ extension DetailLoader {
             throw DataLoadError.failOfMovieDetailInfromationData
         }
         return networkResult
+    }
+    
+    //TODO: - HomeViewModel과 동일한 메서드로 중복 제거 요망
+    func fetchImage(imagePath: String) throws -> Data {
+        
+        let imageURLPath = "\(TMDBBasic.imageURL)\(imagePath)"
+        
+        guard let imageURL = URL(string: imageURLPath) else {
+            throw HomeViewModelInError.failOfMakeURL
+        }
+            
+        guard let imageData = try? Data(contentsOf: imageURL) else {
+            throw HomeViewModelInError.failOfMakeData
+        }
+        
+        return imageData
+    }
+    
+    func convertToMovieInformation(
+        from networkResult: TMDBMovieDetail) throws -> MovieInformation {
+            
+            let wrappingMovieInformation: MovieInformation?
+        
+            var watchGrade: String = ""
+            
+            let nations = networkResult.productionCountries.map { countriesInfo in
+                countriesInfo.name
+            }
+            
+            let genres = networkResult.genres.map { genre in
+                genre.name
+            }
+            
+            if !networkResult.adult {
+                watchGrade = "전체 이용가"
+            }
+            
+            wrappingMovieInformation = MovieInformation(
+                identifier: UUID(),
+                movieSummary: MovieSummary(
+                    watchGrade: watchGrade,
+                    movieKoreanName: networkResult.koreanTitle,
+                    movieEnglishName: networkResult.movieEnglishTitle
+                ),
+                
+                nations: nations,
+                genres: genres,
+                
+                subInformation: SubInformation(
+                    releaseDate: networkResult.releaseDate,
+                    runtime: String(networkResult.runtime),
+                    overview: networkResult.overview
+                )
+            )
+            
+            guard let movieInformation = wrappingMovieInformation else {
+                throw DetailViewModelInError.failOfUnWrapping
+            }
+            return movieInformation
     }
 }
--- MovieApp/MovieApp/Presentation/ViewModel/DetailViewModel.swift
+++ MovieApp/MovieApp/Presentation/ViewModel/DetailViewModel.swift
@@ -91,69 +91,10 @@ extension DetailViewModel {
         
         return movieCastGroup
     }
-    
-    //TODO: - HomeViewModel과 동일한 메서드로 중복 제거 요망
-    private func fetchImage(imagePath: String) async throws -> Data {
-        
-        let imageURLPath = "\(TMDBBasic.imageURL)\(imagePath)"
-        
-        guard let imageURL = URL(string: imageURLPath) else {
-            throw HomeViewModelInError.failOfMakeURL
-        }
-            
-        guard let imageData = try? Data(contentsOf: imageURL) else {
-            throw HomeViewModelInError.failOfMakeData
-        }
-        
-        return imageData
-    }
 }
 
 //MARK: - [Private Method] Help Method
 extension DetailViewModel {
-    
-    private func convertToMovieInformation(
-        from networkResult: TMDBMovieDetail) throws -> MovieInformation {
-            
-            let wrappingMovieInformation: MovieInformation?
-        
-            var watchGrade: String = ""
-            
-            let nations = networkResult.productionCountries.map { countriesInfo in
-                countriesInfo.name
-            }
-            
-            let genres = networkResult.genres.map { genre in
-                genre.name
-            }
-            
-            if !networkResult.adult {
-                watchGrade = "전체 이용가"
-            }
-            
-            wrappingMovieInformation = MovieInformation(
-                identifier: UUID(),
-                movieSummary: MovieSummary(
-                    watchGrade: watchGrade,
-                    movieKoreanName: networkResult.koreanTitle,
-                    movieEnglishName: networkResult.movieEnglishTitle
-                ),
-                
-                nations: nations,
-                genres: genres,
-                
-                subInformation: SubInformation(
-                    releaseDate: networkResult.releaseDate,
-                    runtime: String(networkResult.runtime),
-                    overview: networkResult.overview
-                )
-            )
-            
-            guard let movieInformation = wrappingMovieInformation else {
-                throw DetailViewModelInError.failOfUnWrapping
-            }
-            return movieInformation
-    }
     
     private func convertToMovieCast(from networkResult: TMDBMovieCredit) throws -> [Cast] {
         
