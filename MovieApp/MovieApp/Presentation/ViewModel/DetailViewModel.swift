983681268cb7e77deea6d7f6ffb2331f7dd0bdcd
Jason <ggj00125@gmail.com>
2023-05-30 19:39:24 +0900

    [Feat]: loadSelectedMovieDetailInformation(movieCode:) 메서드 구현

--- MovieApp/MovieApp/Presentation/ViewModel/DetailViewModel.swift
+++ MovieApp/MovieApp/Presentation/ViewModel/DetailViewModel.swift
@@ -18,4 +18,52 @@ final class DetailViewModel {
 
 extension DetailViewModel {
     
+    private func loadSelectedMovieDetailInformation(movieCode: String) async throws -> MovieInformation {
+        
+        var wrappingMovieInformation: MovieInformation?
+        
+        do {
+            let networkResult = try await self.detailLoader.loadMovieDetailInformation(movieCode: movieCode)
+            
+            var watchGrade: String = ""
+            
+            let nations = networkResult.productionCountries.map { countriesInfo in
+                countriesInfo.name
+            }
+            
+            let genres = networkResult.genres.map { genre in
+                genre.name
+            }
+            
+            if !networkResult.adult {
+                watchGrade = "전체 이용가"
+            }
+            
+            wrappingMovieInformation = MovieInformation(
+                identifier: UUID(),
+                movieSummary: MovieSummary(
+                    watchGrade: watchGrade,
+                    movieKoreanName: networkResult.koreanTitle,
+                    movieEnglishName: networkResult.movieEnglishTitle
+                ),
+                
+                nations: nations,
+                genres: genres,
+                
+                subInformation: SubInformation(
+                    releaseDate: networkResult.releaseDate,
+                    runtime: String(networkResult.runtime),
+                    overview: networkResult.overview
+                )
+            )
+        } catch {
+            throw DetailViewModelInError.failOfLoadMovieInformation
+        }
+        
+        guard let movieInformation = wrappingMovieInformation else {
+            throw DetailViewModelInError.failOfUnWrapping
+        }
+        
+        return movieInformation
+    }
 }
