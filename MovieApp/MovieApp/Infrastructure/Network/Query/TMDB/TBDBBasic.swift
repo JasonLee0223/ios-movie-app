a3c8e4c8373b119b22e5b45ec4efb1cda2daf5bd
Jason <ggj00125@gmail.com>
2023-05-30 16:21:28 +0900

    [Feat]: Credit API 요청에 필요한 프로퍼티 추가

--- MovieApp/MovieApp/Infrastructure/Network/Query/TMDB/TBDBBasic.swift
+++ MovieApp/MovieApp/Infrastructure/Network/Query/TMDB/TBDBBasic.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 enum TMDBBasic {
-    static let baseURL: String = "https://api.themoviedb.org/3/trending/movie/"
+    static let trendMovieListBaseURL: String = "https://api.themoviedb.org/3/trending/movie/"
     static let movieDetailBaseURL: String =  "https://api.themoviedb.org/3/movie/"
+    
     static let pathQueryOfWeak: String = "week"
     static let pathQueryOfDay: String = "day"
+    static let pathQueryOfCredit: String = "credits"
     
     static let imageURL: String = "https://image.tmdb.org/t/p/original"
 }
