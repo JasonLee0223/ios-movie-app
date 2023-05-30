fe86f01e1d47dfd471a1b192dee7c7c069764636
Jason <ggj00125@gmail.com>
2023-05-30 20:26:25 +0900

    [Feat]: failOfLoadMovieCast 케이스 추가

--- MovieApp/MovieApp/Presentation/ViewModel/ErrorHandling/DetailViewModelInError.swift
+++ MovieApp/MovieApp/Presentation/ViewModel/ErrorHandling/DetailViewModelInError.swift
@@ -10,11 +10,13 @@ import Foundation
 enum DetailViewModelInError: LocalizedError {
     case failOfUnWrapping
     case failOfLoadMovieInformation
+    case failOfLoadMovieCast
     
     var errorDescription: String? {
         switch self {
         case .failOfUnWrapping: return "❌ Fail Of UnWrapping"
         case .failOfLoadMovieInformation: return "❌ Fail Of Load MovieInformation"
+        case .failOfLoadMovieCast: return "❌ Fail Of LoadMovieCast"
         }
     }
 }
