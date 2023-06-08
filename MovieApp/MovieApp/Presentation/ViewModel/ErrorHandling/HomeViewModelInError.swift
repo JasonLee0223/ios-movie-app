882b76b7220ddef643f8ec1633fc9ef7a235a9cf
Jason <ggj00125@gmail.com>
2023-06-08 16:21:58 +0900

    [Feat]: failOfOptionalUnwrapping 케이스 추가

--- MovieApp/MovieApp/Presentation/ViewModel/ErrorHandling/HomeViewModelInError.swift
+++ MovieApp/MovieApp/Presentation/ViewModel/ErrorHandling/HomeViewModelInError.swift
@@ -10,11 +10,13 @@ import Foundation
 enum HomeViewModelInError: LocalizedError {
     case failOfMakeURL
     case failOfMakeData
+    case failOfOptionalUnwrapping
     
     var errorDescription: String? {
         switch self {
         case .failOfMakeURL: return "❌ Fail Of Make URL"
         case .failOfMakeData: return "❌ Fail Of Make Data"
+        case .failOfOptionalUnwrapping: return "❌ Fail Of bindModels Unwrapping"
         }
     }
 }
