ff650c2d4376cb0bbfd7f78b6a700e6b05024356
Jason <ggj00125@gmail.com>
2023-06-08 16:32:08 +0900

    [Feat]: dataIsEmpty 케이스 추가

--- MovieApp/MovieApp/Presentation/ViewModel/ErrorHandling/HomeViewModelInError.swift
+++ MovieApp/MovieApp/Presentation/ViewModel/ErrorHandling/HomeViewModelInError.swift
@@ -11,12 +11,14 @@ enum HomeViewModelInError: LocalizedError {
     case failOfMakeURL
     case failOfMakeData
     case failOfOptionalUnwrapping
+    case dataIsEmpty
     
     var errorDescription: String? {
         switch self {
-        case .failOfMakeURL: return "❌ Fail Of Make URL"
-        case .failOfMakeData: return "❌ Fail Of Make Data"
-        case .failOfOptionalUnwrapping: return "❌ Fail Of bindModels Unwrapping"
+        case .failOfMakeURL: return "❌ \(#function): Fail Of Make URL"
+        case .failOfMakeData: return "❌ \(#function): Fail Of Make Data"
+        case .failOfOptionalUnwrapping: return "❌ \(#function): Fail Of bindModels Unwrapping"
+        case .dataIsEmpty: return "❌ \(#function): Data Is E.M.P.T.Y"
         }
     }
 }
