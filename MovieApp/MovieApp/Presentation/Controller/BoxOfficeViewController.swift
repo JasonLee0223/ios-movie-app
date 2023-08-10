9f7566fb8bb359761f4ce2f65f64d8d48668ac74
Jasonlee0223 <geonhaeng94@gmail.com>
2023-08-10 14:31:27 +0900

    [Feat]: BoxOffice, Profile ViewController 추가
    TabbarController에 들어갈 ViewController 생성

---
+++ MovieApp/MovieApp/Presentation/Controller/BoxOfficeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BoxOfficeViewController.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/08/10.
+//
+
+import UIKit
+
+final class BoxOfficeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ MovieApp/MovieApp/Presentation/Controller/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/08/10.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
