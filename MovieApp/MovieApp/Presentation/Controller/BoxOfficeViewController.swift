4cf667965a0772d74bc90fffeaaa512c169d9617
Jasonlee0223 <geonhaeng94@gmail.com>
2023-08-11 13:55:24 +0900

    [Feat]: collectionView 생성

--- MovieApp/MovieApp/Presentation/Controller/BoxOfficeViewController.swift
+++ MovieApp/MovieApp/Presentation/Controller/BoxOfficeViewController.swift
@@ -11,6 +11,49 @@ final class BoxOfficeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureOfUI()
+        configureHierarchy()
+    }
+    
+    private let collectionView = UICollectionView(
+        frame: .zero, collectionViewLayout: UICollectionViewLayout()
+    )
+}
+
+
+//MARK: - Configure of View
+extension BoxOfficeViewController {
+    
+    private func configureOfUI() {
+        configureOfSuperView()
+        configureOfCollectionView()
+    }
+    
+    private func configureOfSuperView() {
+        self.view.backgroundColor = .black
+    }
+    
+    private func configureOfCollectionView() {
+        collectionView.isScrollEnabled = true
+        collectionView.clipsToBounds = false
+        collectionView.backgroundColor = .orange
+    }
+}
+
+//MARK: - Configure of Layout
+extension BoxOfficeViewController {
+    
+    private func configureHierarchy() {
+        let safeArea = self.view.safeAreaLayoutGuide
         
+        self.view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
     }
 }
