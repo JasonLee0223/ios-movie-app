d3f90a488898beccb13003664813004b60c029cb
Jasonlee0223 <geonhaeng94@gmail.com>
2023-08-14 14:57:39 +0900

    [Feat]: BoxOfficeSection 타입 구현
    - DiffableDataSource 사용을 위해 구현

---
+++ MovieApp/MovieApp/Presentation/CellLayout/SectionList/BoxOfficeSection.swift
@@ -0,0 +1,12 @@
+//
+//  BoxOfficeSection.swift
+//  MovieApp
+//
+//  Created by Jason on 2023/08/14.
+//
+
+import Foundation
+
+enum BoxOfficeSection: Int, CaseIterable {
+    case boxOffice = 0
+}
