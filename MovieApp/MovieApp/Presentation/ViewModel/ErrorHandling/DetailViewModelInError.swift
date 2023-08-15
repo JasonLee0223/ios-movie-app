e5a2509dafcab7d74595e87f332d300b3e431234
Jasonlee0223 <geonhaeng94@gmail.com>
2023-08-15 15:20:09 +0900

    [Feat]: failedToFindCountryCode 에러 항목 추가

--- MovieApp/MovieApp/Presentation/ViewModel/ErrorHandling/DetailViewModelInError.swift
+++ MovieApp/MovieApp/Presentation/ViewModel/ErrorHandling/DetailViewModelInError.swift
@@ -8,17 +8,17 @@
 import Foundation
 
 enum DetailViewModelInError: LocalizedError {
-    case failOfUnWrapping
+    case failOfUnwrapping
     case failOfLoadMovieInformation
     case failOfLoadMovieCast
-    case failOfUnwrapping
+    case failedToFindCountryCode
     
     var errorDescription: String? {
         switch self {
-        case .failOfUnWrapping: return "❌ Fail Of UnWrapping"
+        case .failOfUnwrapping: return "❌ Fail Of UnWrapping"
         case .failOfLoadMovieInformation: return "❌ Fail Of Load MovieInformation"
         case .failOfLoadMovieCast: return "❌ Fail Of LoadMovieCast"
-        case .failOfUnwrapping: return "❌ Fail Of Unwrapping"
+        case .failedToFindCountryCode: return "❌ 국가 코드에 해당하는 국가를 찾을 수 없습니다."
         }
     }
 }
