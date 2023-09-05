095bff31023407f55a2950eabf37d58ef92fdf63
Jasonlee0223 <geonhaeng94@gmail.com>
2023-09-05 15:25:53 +0900

    [Refactor]: 오픈소스 라이브러리 추가 및 DTO 타입 파일 분리를 다시 합침
    - Snapkit
    - RxSwift, RxRelay, RxCocoa 추가

--- MovieApp/MovieApp/Data/DataStructure/Response/BoxOffice/BoxOffice.swift
+++ MovieApp/MovieApp/Data/DataStructure/Response/BoxOffice/BoxOffice.swift
@@ -9,4 +9,43 @@ import Foundation
 
 struct BoxOffice: Decodable {
     let boxOfficeResult: BoxOfficeResult
+    
+    struct BoxOfficeResult: Decodable {
+        let boxofficeType: String
+        let dateRange: String
+        let dailyBoxOfficeList: [DailyBoxOfficeList]
+
+        enum CodingKeys: String, CodingKey {
+            case boxofficeType
+            case dailyBoxOfficeList
+            case dateRange = "showRange"
+        }
+    }
+}
+
+struct DailyBoxOfficeList: Decodable {
+    let rank: String
+    let rankOldAndNew: RankOldAndNew
+    let rankVariation: String
+    let movieCode: String
+    let movieName: String
+    let openDate: String
+    let audienceCount: String
+    let audienceAccumulate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case rank
+        case rankOldAndNew
+        case rankVariation = "rankInten"
+        case movieCode = "movieCd"
+        case movieName = "movieNm"
+        case openDate = "openDt"
+        case audienceCount = "audiCnt"
+        case audienceAccumulate = "audiAcc"
+    }
+}
+
+enum RankOldAndNew: String, Decodable {
+    case new = "NEW"
+    case old = "OLD"
 }
