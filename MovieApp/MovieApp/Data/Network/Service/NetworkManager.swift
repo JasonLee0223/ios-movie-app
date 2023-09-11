df855ae75156bacbd84021e9e43b425a8ba69f92
Jasonlee0223 <geonhaeng94@gmail.com>
2023-09-11 19:41:22 +0900

    [Refactor]: API를 통해 받아오는 각각의 메서드를 하나의 loadAPIData메서드로 추상화 구현

--- MovieApp/MovieApp/Data/Network/Service/NetworkManager.swift
+++ MovieApp/MovieApp/Data/Network/Service/NetworkManager.swift
@@ -11,87 +11,21 @@ import RxSwift
 
 final class NetworkManager: Gettable {
     
-    
-    /// TMDB
-    func loadTrendMovieList(path: MakeQueryItem.SubPath) -> Single<[Result]> {
+    func loadAPIData<M: Decodable, P: Encodable>(_ url: URL, parameters: P) -> Single<M> {
         
-        let url = MakeQueryItem.trendMovie(path).url
-        let parameters = TMDBQueryParameters(language: getLangauge, key: getAPIKEY(type: .TMDB))
-        
-        return Single<[Result]>.create { single in
+        return Single<M>.create { single in
             AF.request(url, method: .get, parameters: parameters)
-                .responseData { response in
+            .responseData { response in
                 switch response.result {
                 case .success(let data):
                     do {
-                        let result = try self.decoder.decode(TMDBTrendMovieList.self, from: data)
-                        single(.success(result.results))
+                        let result: M = try self.decoder.decode(M.self, from: data)
+                        single(.success(result))
                     } catch {
-                        print(DataLoadError.loadFailOfTrendMovieListData)
+                        single(.failure(error))
                     }
                 case .failure(let error):
-                    print(error)
-                    break
-                }
-            }
-            return Disposables.create()
-        }
-    }
-    
-    func loadMovieDetailInformation(movieCode: String, completionHandler: @escaping (TMDBMovieDetail) -> Void) {
-        
-        let urlArray = [MakeQueryItem.detail(movieCode).url, MakeQueryItem.credit(movieCode, .credit).url]
-        let group = DispatchGroup()
-        
-        urlArray.forEach { url in
-            
-            let parameters = TMDBQueryParameters(language: getLangauge, key: getAPIKEY(type: .TMDB))
-            
-            AF.request(url, method: .get, parameters: parameters)
-                .responseData { response in
-                    switch response.result {
-                    case .success(let data):
-                        do {
-                            let result = try self.decoder.decode(TMDBMovieDetail.self, from: data)
-                            completionHandler(result)
-                        } catch {
-                            print(DataLoadError.loadFailOfMovieDetailInfromationData)
-                        }
-                    case .failure(let error):
-                        print(error)
-                        break
-                    }
-                }
-            group.leave()
-        }
-        
-        group.notify(queue: .global()) {
-            print("All Request Completed")
-        }
-    }
-    
-    /// BoxOffice
-    func loadBoxOffice() -> Single<[DailyBoxOfficeList]> {
-        let url = MakeQueryItem.boxOffice.url
-        let parameters = BoxOfficeQueryParameters(
-            key: getAPIKEY(type: .KOFIC),
-            targetDate: getCurrentDate().split(separator: "-").joined()
-        )
-        
-        return Single<[DailyBoxOfficeList]>.create { single in
-            AF.request(url, method: .get, parameters: parameters)
-                .responseData { response in
-                switch response.result {
-                case .success(let data):
-                    do {
-                        let result = try self.decoder.decode(BoxOffice.self, from: data)
-                        single(.success(result.boxOfficeResult.dailyBoxOfficeList))
-                    } catch {
-                        print(DataLoadError.loadFailOfBoxOfficeList)
-                    }
-                case .failure(let error):
-                    print(error)
-                    break
+                    single(.failure(error))
                 }
             }
             return Disposables.create()
