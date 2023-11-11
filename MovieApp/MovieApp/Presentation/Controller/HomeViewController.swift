07ab09e8d87c829233532c37e696ec9355c01825
Jasonlee0223 <geonhaeng94@gmail.com>
2023-11-11 17:27:50 +0900

    [Style]: 잘못 구현한 초기화 내용 수정

--- MovieApp/MovieApp/Presentation/Controller/HomeViewController.swift
+++ MovieApp/MovieApp/Presentation/Controller/HomeViewController.swift
@@ -13,15 +13,6 @@ import SnapKit
 
 final class HomeViewController: UIViewController {
     
-    init(viewModel: HomeViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
